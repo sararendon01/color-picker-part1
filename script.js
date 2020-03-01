3549886f657aa86bb478de7cd2aaf20320293757
srendon <srendon4@cnm.edu>
2020-03-01 08:32:21 -0700

    color-picker

--- script.js
+++ script.js
@@ -7,19 +7,37 @@ function initcolorpicker() {
 	let rgb = {
 		red: document.getElementById("red"),
 		green: document.getElementById("green"),
-		blue: document.getElementById("blue")
+		blue: document.getElementById("blue"),
 	};
-	setColorPickerEventListeners(colorBox, rgb);
+	let colorPickers = document.getElementsByClassName("picker");
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb) {
-		rgb.red.addEventListener('change', () => {
-			console.log("Red value: ", rgb.red.value);
+function setColorPickerEventListeners(element, colors, pickerElements) {
+	let pickerlen = pickerElement.length;
+	for (let i = 0; i < pickerlen; i++){
+		pickerElements[i].addEventListener('change',() =>{
+			let red = colors.red.value;
+			let green = colors.green.value;
+			let blue = colors.blue.value;
+	setElemtBGColor(element, red, green, blue);
+	setDisplayValues(red, green, blue)
 		});
-		rgb.green.addEventListener('change', () => {
-			console.log("green value: ", rgb.green.value);
-		});
-		rgb.blue.addEventListener('change', () => {
-			console.log("blue value: ", rgb.blue.value);
-		});
+	}
+}
+
+function setElementBGColor(bgElement, red, green, blue) {
+	let rgbVal =[red, green, blue]. join(',');
+	bgElement.style.backgroundColor = "rgb(" +rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue) {
+	let redval = document. getElementById("redval");
+	let greenval = document. getElementById("greenval");
+	let blueval = document. getElementById("blueval");
+
+	redval.innerText = red;
+	greenval.innerText = green;
+	blueval.innerText = blue;
+
 }
